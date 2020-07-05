98043cd806401a724baffef2a5535103c41713b3
Parmentier Nicholas <parmentiernicholas@gmail.com>
2020-07-05 13:56:34 +0200

    Initial Commit

---
+++ main.py
@@ -0,0 +1,31 @@
+import requests
+import configparser
+
+# Get own ip
+ip = requests.get('https://api.ipify.org?format=json').json()["ip"]
+
+print(ip)
+
+## Get zone
+cp = configparser.RawConfigParser()
+configFilePath = './config.cfg'
+cp.read(configFilePath)
+
+name = cp.get('main-config', 'name') 
+email= cp.get('main-config', 'email') 
+key = cp.get('main-config', 'key') 
+
+print(name)
+
+head = {'X-Auth-Email': email, 'X-Auth-Key': key}
+zone_id = requests.get("https://api.cloudflare.com/client/v4/zones?name=" + name, headers=head).json()['result'][0]['id']
+
+print(zone_id)
+
+dns_records = requests.get("https://api.cloudflare.com/client/v4/zones/" + zone_id + "/dns_records", headers=head).json()['result']
+
+for record in dns_records:
+    if record['type'] == 'A':
+        data = {'type':record['type'],'name':record['name'],'content':ip,'ttl':record['ttl'],'proxied':record['proxied']}
+        update_record = requests.put("https://api.cloudflare.com/client/v4/zones/" + zone_id + "/dns_records/"+ record["id"], headers=head, json=data).json()
+        print(update_record)
